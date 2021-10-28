c5cd275bb39933b24aa6ecc13d4cfc9a30d23c0e
Ismael Morel <ismaelmorel84@gmail.com>
2021-10-28 01:59:58 -0300

    logica hecha

---
+++ src/index.js
@@ -0,0 +1,55 @@
+
+const d = document;
+
+const w = window;
+
+const btn = d.querySelector('#btn-search');
+
+const username = d.querySelector('#username'); 
+
+
+
+const printer = ({avatar, name, twitter}) => {
+    const div = d.querySelector('#print-user');
+    div.innerHTML = `
+        <img class="img-avatar" src=${avatar}/>
+        <h2 class="user-name">${name}</h2>
+        <h2 class="user-twitter">${twitter}</h2>
+    `
+}
+const repoPrint = ({repoName, url}) => {
+    const div = d.querySelector('#print-user');
+    div.innerHTML += `
+        <h3 class="repo-name">${repoName}</h3>
+        <h3 class="repo-url"><a href=${url}>${url}</a></h3>
+    `
+}
+
+const lista = [];
+btn.addEventListener('click', () => {
+    let realUser = username.value.split(' ').join('')
+    setTimeout(() => {
+        fetch(`https://api.github.com/users/${realUser}`)
+            .then((response) => response.json())
+            .then(data => {
+                printer({
+                    avatar : data.avatar_url,
+                    name : data.name,
+                    twitter : data.twitter_username
+                })
+                fetch(data.repos_url)
+                .then((result) => result.json())
+                .then((repos) => repos.map((info) => {
+                    return repoPrint({
+                        repoName : info.name,
+                        url : info.html_url
+                    })
+                }))
+            })
+            console.log(lista)
+            
+    }, 2000)
+    
+})
+
+
